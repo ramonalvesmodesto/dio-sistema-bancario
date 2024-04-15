9d49a127fabe60725a5b72c8099dba6e31813777
ramonalvesmodesto2 <ramonmodesto16@protonmail.com>
2024-04-15 16:26:19 -0300

    Adicionado funções de depósito, saque e exibir extrato

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -1,10 +1,57 @@
 saldo_conta = 0
 valor_deposito = 0
 limite_saque_diario = 3
-lista_deposito = []
-lista_saque = []
+numero_saques = limite_saque_diario
+extrato_deposito = []
+extrato_saque = []
 
 LIMITE_SAQUE = 500
+
+def saque(saldo, valor, extrato, limite, numero_saques, limite_saques,/):
+    calculo_saldo = saldo - valor
+    novo_saldo = saldo
+    extrato_saque = extrato.copy()
+
+    if saldo == 0:
+        print('Você não possui saldo!')
+    elif numero_saques == 0:
+        print(f'Seu limite de saque diário foi excedido! Seu limite de saque é {limite_saques}, e você realizou esgotou seus saques diários')
+    elif calculo_saldo < 0:            
+        print('Não foi possível autorizar o saque! O valor de saque excedeu seu saldo!')
+    elif valor > limite:
+            print(f'Não foi possível realizar o saque! Seu limite de saque é: R${limite: .2f}')
+    else:
+        novo_saldo = saldo - valor
+        extrato_saque.append(valor)
+        
+    return novo_saldo, extrato_saque
+
+def deposito (*, saldo, valor, extrato):
+    novo_saldo = saldo
+    extrato_deposito = extrato.copy()
+
+    novo_saldo = saldo + valor
+    extrato_deposito.append(valor_deposito)
+
+    return novo_saldo, extrato_deposito
+
+def exibir_extrato (deposito, saque):
+    print('\n\nDepósitos: \n')
+    
+    if len(deposito) == 0:
+        print('Não foram realizadas movimentações!')
+    else:
+        for valor in deposito:
+            print(f'R${valor: .2f}')
+        
+    print('\nSaques: \n')
+    
+    if len(saque) == 0:
+        print('Não foram realizadas movimentações!')
+    else:
+        for valor in saque:
+            print(f'R$ -{valor: .2f}')
+
 
 while True:
     print(f'''
@@ -22,49 +69,30 @@ while True:
     
     if entrada == '1':
         valor_deposito = float(input('Digite o valor para depósito: '))
-        
-        if valor_deposito > 0:
-            saldo_conta += valor_deposito
-            lista_deposito.append(valor_deposito)
-        else:
+
+        if valor_deposito < 0:
             print('Não é permitido depósitos de valores iguais ou menores que zero!')
             continue
+
+        resultado = deposito(saldo=saldo_conta, valor=valor_deposito, extrato=extrato_deposito)
+        saldo_conta = resultado[0]
+        extrato_deposito = resultado[1].copy()
+          
     
     if entrada == '2':
-        if saldo_conta == 0:
-            print('Você não possui saldo!')
-        elif limite_saque_diario == 0:
-            print('Seu limite de saque diário foi excedido! Tente novamente amanhã!')
-        else:            
-            valor_saque = int(input('Digite o valor de saque: '))
-            calculo_saldo = saldo_conta - valor_saque
-            
-            if calculo_saldo < 0:
-                print('Não foi possível altorizar o saque! O valor de saque excedeu seu saldo!')
-            elif valor_saque > 500:
-                print(f'Não foi possível realizar o saque! Seu limite de saque é: R${LIMITE_SAQUE: .2f}')
-            else:
-                saldo_conta -= valor_saque
-                limite_saque_diario -= 1
-                lista_saque.append(valor_saque)
+        valor_saque = int(input('Digite o valor de saque: '))
+        resultado = saque(saldo_conta, valor_saque, extrato_saque, LIMITE_SAQUE, numero_saques, limite_saque_diario)
+        if resultado[0] < saldo_conta:
+            numero_saques -= 1
+            saldo_conta = resultado[0]
+            extrato_saque = resultado[1].copy()
     
     if entrada == '3':
-        print('\n\nDepósitos: \n')
-        
-        if len(lista_deposito) == 0:
-            print('Não foram realizadas movimentações!')
-        else:
-            for valor in lista_deposito:
-                print(f'R${valor: .2f}')
-            
-        print('\nSaques: \n')
-        
-        if len(lista_saque) == 0:
-            print('Não foram realizadas movimentações!')
-        else:
-            for valor in lista_saque:
-                print(f'R$ -{valor: .2f}')
+        exibir_extrato(extrato_deposito, extrato_saque)
                 
     if entrada == 'q':
         break
+
+
+
             
