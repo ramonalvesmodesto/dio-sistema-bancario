a1899749ad8becc4f95f6d05c8fdc6827c57b94f
ramonalvesmodesto2 <ramonmodesto1698@gmail.com>
2024-04-24 23:56:47 -0300

    Implementado classe PessoaFisica

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -113,6 +113,26 @@ class Cliente:
         self._contas.append(conta)
 
 
+class PessoaFisica(Cliente):
+    def __init__(self, endereco, cpf, nome, data_nascimento):
+        super().__init__(endereco)
+        self._cpf = cpf
+        self._nome = nome 
+        self._data_nascimento = data_nascimento
+
+
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def data_nascimento(self):
+        return self._data_nascimento
+
 
 def saque (saldo, valor, extrato, limite, numero_saques, limite_saques,/):
     calculo_saldo = saldo - valor
