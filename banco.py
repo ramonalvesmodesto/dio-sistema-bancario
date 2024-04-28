89b1b9feec6f15c8659b1850d983a96afedfcba2
ramonalvesmodesto2 <ramonmodesto1698@gmail.com>
2024-04-28 12:06:04 -0300

    Refatoração: Formatação usando black, isort e flake8

--- banco.py
+++ banco.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-from abc import ABC, abstractmethod
-
 import textwrap
+from abc import ABC, abstractmethod
+from datetime import datetime
+
 
 class ContaIterador:
     def __init__(self, contas):
@@ -14,29 +14,32 @@ class ContaIterador:
     def __next__(self):
         try:
             conta = self._contas[self._contador]
-            return textwrap.dedent(f'''
+            return textwrap.dedent(
+                f"""
                 Nome: {conta.cliente.nome}
                 C/C: {conta.numero}
                 Agência: {conta.agencia}
                 Saldo: {conta.saldo}
-            ''')
+            """
+            )
         except IndexError:
-            raise StopIteration 
+            raise StopIteration
         finally:
             self._contador += 1
+
 
 class Conta:
     def __init__(self):
         self._saldo = 0
-        self._numero = ''
-        self._agencia = '0001'
-        self._cliente = ''
+        self._numero = ""
+        self._agencia = "0001"
+        self._cliente = ""
         self._historico = Historico()
 
     @property
     def saldo(self):
         return self._saldo
-    
+
     @saldo.setter
     def saldo(self, valor):
         self._saldo += valor
@@ -48,15 +51,15 @@ class Conta:
     @property
     def numero(self):
         return self._numero
-    
+
     @property
     def agencia(self):
         return self._agencia
-    
+
     @property
     def cliente(self):
         return self._cliente
-    
+
     @property
     def historico(self):
         return self._historico
@@ -64,31 +67,38 @@ class Conta:
     def nova_conta(self, cliente, numero):
         self._cliente = cliente
         self._numero = numero
-        print('\nConta criada com sucesso!!\n')
+        print("\nConta criada com sucesso!!\n")
 
     def sacar(self, valor):
         calculo_saldo = self._saldo - valor
         if calculo_saldo < 0:
-            print('\nNão foi possível autorizar o saque! O valor de saque excedeu seu saldo!\n')
+            print(
+                "\nNão foi possível autorizar o saque! O valor de saque excedeu seu saldo!\n"
+            )
             return False
         elif valor > self._limite:
-            print(f'\nNão foi possível realizar o saque! Seu limite de saque é: R${self._limite: .2f}')
+            print(
+                f"\nNão foi possível realizar o saque! Seu limite de saque é: R${self._limite: .2f}"
+            )
             return False
-        
+
         return True
 
     def depositar(self, valor):
         if self._saldo + valor <= self._saldo:
-            print('\nNão é permitido depósitos de valores iguais ou menores que zero!\n')
+            print(
+                "\nNão é permitido depósitos de valores iguais ou menores que zero!\n"
+            )
             return False
-        
+
         return True
-    
+
     def __str__(self):
         return f"Agência: {self._agencia} C/C: {self._numero}"
-    
+
+
 class ContaCorrente(Conta):
-    def __init__(self, limite = 500.0, limite_saques = 10):
+    def __init__(self, limite=500.0, limite_saques=10):
         super().__init__()
         self._limite = limite
         self._limite_saques = limite_saques
@@ -96,11 +106,11 @@ class ContaCorrente(Conta):
     @property
     def limite(self):
         return self._limite
-    
+
     @property
     def limite_saques(self):
         return self._limite_saques
-    
+
     def alterar_limite_saques(self, novo_limite_saque):
         self._limite_saques = novo_limite_saque
 
@@ -109,21 +119,23 @@ class ContaCorrente(Conta):
 
     def __repr__(self):
         return f"<{self.__class__.__name__}: ('{self.agencia}', '{self.numero}', '{self.cliente.nome}')>"
-    
+
+
 class Transacao(ABC):
     @abstractmethod
     def registrar(conta: Conta):
         pass
 
+
 class Deposito(Transacao):
     def __init__(self, valor=0.0):
         self._valor = valor
-        self._data_hora_transacao = datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
+        self._data_hora_transacao = datetime.now().strftime("%d-%m-%Y, %H:%M:%S")
 
     @property
     def valor(self):
         return self._valor
-    
+
     @valor.setter
     def valor(self, valor):
         self._valor = valor
@@ -131,28 +143,29 @@ class Deposito(Transacao):
     @property
     def data_hora_transacao(self):
         return self._data_hora_transacao
-    
+
     def registrar(self, conta: Conta):
         if conta.depositar(self._valor):
             conta.historico.adicionar_transacao(Deposito(self._valor))
             conta.saldo = self.valor
-            print('\nDepósito realizado com sucesso!!\n')
+            print("\nDepósito realizado com sucesso!!\n")
 
     def __str__(self):
         return f"Depósito: +{self._valor:.2f} - {self._data_hora_transacao}"
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__}: ('{self.data_hora_transacao}')>"
+
 
 class Saque(Transacao):
     def __init__(self, valor=0.0):
         self._valor = valor
-        self._data_hora_transacao = datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
+        self._data_hora_transacao = datetime.now().strftime("%d-%m-%Y, %H:%M:%S")
 
     @property
     def valor(self):
         return self._valor
-    
+
     @valor.setter
     def valor(self, valor):
         self._valor = valor
@@ -160,19 +173,20 @@ class Saque(Transacao):
     @property
     def data_hora_transacao(self):
         return self._data_hora_transacao
-    
+
     def registrar(self, conta: Conta):
         if conta.sacar(self._valor):
             conta.historico.adicionar_transacao(Saque(self._valor))
             conta.saldo = -self._valor
-            print('\nSaque realizado com sucesso!!\n')
+            print("\nSaque realizado com sucesso!!\n")
 
     def __str__(self):
         return f"Saque: -{self._valor:.2f} - {self._data_hora_transacao}"
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__}: ('{self.data_hora_transacao}')>"
-    
+
+
 class Historico:
     def __init__(self):
         self._transacoes = []
@@ -186,24 +200,34 @@ class Historico:
 
     def gerar_relatorio(self, tipo_transacao=None):
         for transacao in self._transacoes:
-            if tipo_transacao == str(transacao.__class__.__name__): yield transacao
-            elif tipo_transacao == str(transacao.__class__.__name__): yield transacao
-            elif tipo_transacao == None: yield transacao
+            if tipo_transacao == str(transacao.__class__.__name__):
+                yield transacao
+            elif tipo_transacao == str(transacao.__class__.__name__):
+                yield transacao
+            elif tipo_transacao is None:
+                yield transacao
 
     def transacoes_do_dia(self, tipo_transacao):
         data_atual = datetime.now().date()
         transacoes = []
 
         for transacao in self._transacoes:
-            if datetime.strptime(transacao.data_hora_transacao, "%d-%m-%Y, %H:%M:%S").date() == data_atual and transacao.__class__.__name__ == tipo_transacao:
+            if (
+                datetime.strptime(
+                    transacao.data_hora_transacao, "%d-%m-%Y, %H:%M:%S"
+                ).date()
+                == data_atual
+                and transacao.__class__.__name__ == tipo_transacao
+            ):
                 transacoes.append(transacao)
         return transacoes
 
     def __str__(self) -> str:
         return f"\nExtrato: \n{', '.join([f'{transacao}' for transacao in self.transacoes])}"
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__}: ('{self._transacoes}')>"
+
 
 class Endereco:
     def __init__(self, logradouro, numero, estado, cidade, bairro):
@@ -216,66 +240,70 @@ class Endereco:
     @property
     def logradouro(self):
         return self._logradouro
-    
+
     @property
     def numero(self):
         return self._numero
-    
+
     @property
     def estado(self):
         return self._estado
-    
+
     @property
     def cidade(self):
         return self._cidade
-    
+
     @property
     def bairro(self):
         return self._bairro
-    
+
     def __str__(self):
-        return f"Logradouro: {self._logradouro} \nNúmero: {self._numero} \nEstado: {self._estado} \nCidade: {self._cidade} \nBairro: {self._bairro}"
+        return f"Logradouro: {self._logradouro} \nNúmero: {self._numero} \nEstado: {self._estado} \
+            \nCidade: {self._cidade} \nBairro: {self._bairro}"
+
 
 class Cliente:
-    def __init__(self, endereco: Endereco = ''):
+    def __init__(self, endereco: Endereco = ""):
         self._endereco = endereco
         self._contas = []
-        self._conta_principal = ''
+        self._conta_principal = ""
 
     @property
     def endereco(self):
         return self._endereco
-    
+
     @endereco.setter
     def endereco(self, endereco):
         self._endereco = endereco
-    
+
     @endereco.deleter
     def endereco(self):
-        self._endereco = ''
+        self._endereco = ""
 
     @property
     def contas(self):
         return self._contas
-    
+
     @property
     def conta_principal(self):
         return self._conta_principal
-    
+
     @conta_principal.setter
     def conta_principal(self, conta):
         self._conta_principal = conta
 
     @conta_principal.deleter
     def conta_principal(self):
-        self._conta_principal = ''
-    
+        self._conta_principal = ""
+
     def realizar_transacao(self, conta: Conta, transacao: Transacao):
         tipo_transacao = transacao.__class__.__name__
         transacoes_do_dia = conta.historico.transacoes_do_dia(tipo_transacao)
-        
-        if len(transacoes_do_dia) < conta.limite_saques: transacao.registrar(conta)
-        else: print(f"\nVocê excedeu o limite de {conta.limite_saques} {tipo_transacao}s")
+
+        if len(transacoes_do_dia) < conta.limite_saques:
+            transacao.registrar(conta)
+        else:
+            print(f"\nVocê excedeu o limite de {conta.limite_saques} {tipo_transacao}s")
 
     def adicionar_conta(self, conta: Conta):
         self._contas.append(conta)
@@ -283,32 +311,33 @@ class Cliente:
     def __str__(self) -> str:
         return f"{self._endereco}, {self._conta_principal}, {self._contas}"
 
+
 class PessoaFisica(Cliente):
-    def __init__(self, cpf, nome, data_nascimento, endereco: Endereco=''):
+    def __init__(self, cpf, nome, data_nascimento, endereco: Endereco = ""):
         super().__init__(endereco)
         self._cpf = cpf
-        self._nome = nome 
+        self._nome = nome
         self._data_nascimento = data_nascimento
-
 
     @property
     def cpf(self):
         return self._cpf
-    
+
     @property
     def nome(self):
         return self._nome
-    
+
     @property
     def data_nascimento(self):
         return self._data_nascimento
-    
+
     def __str__(self):
         return f"{', '.join([f'{conta}' for conta in self._contas])}\n"
-    
+
     def __repr__(self):
         return f"<{self.__class__.__name__}: ('{self.cpf}')>"
-    
+
+
 class Banco:
     def __init__(self):
         self._ultimo_valor_deposito = 0
@@ -316,7 +345,7 @@ class Banco:
         self._id_cliente_logado = None
         self._cliente_logado = Cliente()
         self._numero_conta_corrente = 1
-        self._conta_corrente_cliente_sessao_logada: Conta = ''
+        self._conta_corrente_cliente_sessao_logada: Conta = ""
 
     @property
     def ultimo_valor_deposito(self):
@@ -325,26 +354,26 @@ class Banco:
     @property
     def clientes(self):
         return self._clientes
-    
+
     @clientes.setter
     def clientes(self, cliente: Cliente):
         self._clientes.append(cliente)
-    
+
     @property
     def id_cliente_logado(self):
         return self._id_cliente_logado
-    
+
     @id_cliente_logado.setter
     def id_cliente_logado(self, id):
         self._id_cliente_logado = id
-    
+
     @property
     def cliente_logado(self):
         return self._cliente_logado
-    
+
     @cliente_logado.setter
     def cliente_logado(self, cliente: Cliente):
-        if cliente != None:
+        if cliente is not None:
             self._cliente_logado = cliente
             self._id_cliente_logado = cliente.cpf
             self._conta_corrente_cliente_sessao_logada = cliente.conta_principal
@@ -352,31 +381,30 @@ class Banco:
     @property
     def numero_conta_corrente(self):
         return self._numero_conta_corrente
-    
+
     @numero_conta_corrente.setter
     def numero_conta_corrente(self, numero):
         self._numero_conta_corrente += numero
 
     @property
-    def conta_corrente_cliente_sessao_logada (self):
+    def conta_corrente_cliente_sessao_logada(self):
         return self._conta_corrente_cliente_sessao_logada
-    
+
     @conta_corrente_cliente_sessao_logada.setter
-    def conta_corrente_cliente_sessao_logada (self, conta_corrente):
+    def conta_corrente_cliente_sessao_logada(self, conta_corrente):
         self._conta_corrente_cliente_sessao_logada = conta_corrente
 
     def buscar_cliente(self, cpf):
         for cliente in self._clientes:
             if cliente.cpf == cpf:
                 return cliente
-        
+
         return None
-    
+
     def __str__(self):
-        return f"{self._cliente_logado}, {self.ultimo_valor_deposito}, {self._clientes}, {self._id_cliente_logado}, {self._numero_conta_corrente}, {self._conta_corrente_cliente_sessao_logada}"
-    
+        return f"{self._cliente_logado}, {self.ultimo_valor_deposito}, {self._clientes}, {self._id_cliente_logado}, \
+            {self._numero_conta_corrente}, {self._conta_corrente_cliente_sessao_logada}"
+
     def __repr__(self):
-        return f"<{self.__class__.__name__}: ('{self.numero_conta_corrente}', '{self.conta_corrente_cliente_sessao_logada}')>"
-
-
-                
+        return f"<{self.__class__.__name__}: ('{self.numero_conta_corrente}', \
+                '{self.conta_corrente_cliente_sessao_logada}')>"
--- menu.py
+++ menu.py
@@ -1,39 +1,42 @@
 import textwrap
 
-def mostrar_menu_login_cadastro ():
-    menu = '''
-    ================ Menu ================         
+
+def mostrar_menu_login_cadastro():
+    menu = """
+    ================ Menu ================
     [1] - Fazer login
     [2] - Realizar cadastro
     [3] - Exibir contas clientes do banco
-    [q] - Sair 
+    [q] - Sair
     =======================================
-    '''
+    """
     print(textwrap.dedent(menu))
 
-def mostrar_menu_extrato ():
-    menu = '''
-    =============== Menu ===============         
+
+def mostrar_menu_extrato():
+    menu = """
+    =============== Menu =================
     [1] - Depósito
     [2] - Saque
-    [3] - Saque e depósito 
+    [3] - Saque e depósito
     ======================================
-    '''
+    """
     print(textwrap.dedent(menu))
 
-def mostrar_menu_movimentacao_conta (saldo, deposito, usuario):
-    menu = f'''\n
+
+def mostrar_menu_movimentacao_conta(saldo, deposito, usuario):
+    menu = f"""\n
     =============== Menu ===============
-    Saldo: R${saldo: .2f}     
+    Saldo: R${saldo: .2f}
     Último Depósito: R${deposito: .2f}
     Usuário: {usuario}
 
-    [1] - \tDepósito       
-    [2] - \tSaque          
+    [1] - \tDepósito
+    [2] - \tSaque
     [3] - \tExtrato
-    [4] - \tCriar Conta   
-    [5] - \tExibir Contas     
+    [4] - \tCriar Conta
+    [5] - \tExibir Contas
     [q] - \tSair
-    ======================================         
-    '''
+    ======================================
+    """
     print(textwrap.dedent(menu))
