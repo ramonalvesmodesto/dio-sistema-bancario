8f9d98c961fb8f5494c92494703a1e5a95d4bd92
ramonalvesmodesto2 <ramonmodesto1698@gmail.com>
2024-05-01 06:46:32 -0300

    Refatoração mvc pessoa física

---
+++ src/model/pessoaFisica.py
@@ -0,0 +1,29 @@
+from model.cliente import ClienteModel
+from model.endereco import EnderecoModel
+
+
+class PessoaFisicaModel(ClienteModel):
+    def __init__(self, cpf, nome, data_nascimento, endereco: EnderecoModel = ""):
+        super().__init__(endereco)
+        self._cpf = cpf
+        self._nome = nome
+        self._data_nascimento = data_nascimento
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def data_nascimento(self):
+        return self._data_nascimento
+    
+    def __str__(self):
+        return f"{', '.join([f'{conta}' for conta in self._contas])}\n"
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: ('{self.cpf}')>"
+
